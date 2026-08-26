0746a35bc847c24099fc6697e17eb04424575a0d
Srushtipatil0004 <patilsrushti0004@gmail.com>
2026-08-27 03:26:20 +0530

    Fix duplicate dashboard header

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,12 @@
-import Header from "../components/Header";
 import AppointmentsCard from "../components/AppointmentsCard";
 import MedicinesCard from "../components/MedicinesCard";
 
 function Dashboard() {
   return (
-    <>
-      <Header />
-      <section className="dashboard-grid">
-        <AppointmentsCard />
-        <MedicinesCard />
-      </section>
-    </>
+    <section className="dashboard-grid">
+      <AppointmentsCard />
+      <MedicinesCard />
+    </section>
   );
 }
 
